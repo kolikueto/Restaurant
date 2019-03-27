db8f8fe6f551774ead9f5d39509d1ef97cb9e372
Mabraxas <mpietragallo@gmail.com>
2019-03-27 13:46:43 -0300

    GetDate Javascript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-<script>
+
 n =  new Date();
 y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-document.getElementById("fechaloca").innerHTML = m + "/" + d + "/" + y;
-</script>
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+document.getElementById("fechaactual").innerHTML = dias[n.getDay()] + " " + n.getDate() +" de "+ meses[n.getMonth()] + " de " + y;
+ 
